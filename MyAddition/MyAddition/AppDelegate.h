454c1fa033769d7883c8718851a7c71c8e33429c
蔡成汉 <1178752402@qq.com>
2015-05-07 14:48:57 +0800

    no message

---
+++ MyAddition/MyAddition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyAddition
+//
+//  Created by 蔡成汉 on 15/5/7.
+//  Copyright (c) 2015年 JW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyAddition/MyAddition/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyAddition
+//
+//  Created by 蔡成汉 on 15/5/7.
+//  Copyright (c) 2015年 JW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
